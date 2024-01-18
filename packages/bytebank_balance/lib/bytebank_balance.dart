784610be84d78b26511809bb13fe40af4c841d37
Joao Victor Franco Rosa <joaovictorfrancorosa@gmail.com>
2024-01-18 17:04:04 -0300

    first commit

---
+++ packages/bytebank_balance/lib/bytebank_balance.dart
@@ -0,0 +1,7 @@
+library bytebank_balance;
+
+import 'package:flutter/material.dart';
+import 'service/balance_service.dart';
+import 'widgets/type_pin_dialog.dart';
+
+part 'widgets/bytebank_balance.dart';
